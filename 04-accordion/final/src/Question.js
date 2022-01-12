3f4e4e0247c3c130fad9eb72dcadfd740200408f
Zieho Choi <jeff426@naver.com>
2022-01-12 17:20:21 +0900

    conditional rendering ? True: false, !prev, &&operator

--- 04-accordion/final/src/Question.js
+++ 04-accordion/final/src/Question.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
 const Question = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
