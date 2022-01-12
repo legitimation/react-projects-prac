571207c1597b85daa5329d6c61aa032833532d18
Zieho Choi <jeff426@naver.com>
2022-01-12 17:08:17 +0900

    prev, next, random, when next from last return first

--- 03-reviews/final/src/Review.js
+++ 03-reviews/final/src/Review.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+  // to allow to come back to index 0 when moving next from the last comp, and to redirect to last comp when moving prev from first comp.
+  // check num is used in both, next, prev, random person so therefore, reused components can be used as a func to show which prevent code duplication
   const checkNumber = (number) => {
     if (number > people.length - 1) {
       return 0;
@@ -13,18 +16,22 @@ const Review = () => {
     }
     return number;
   };
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
       return checkNumber(newIndex);
     });
   };
+
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
       return checkNumber(newIndex);
     });
   };
+
+  // randomise, if random returns same val add 1 
   const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
     if (randomNumber === index) {
