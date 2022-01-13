5a6d65eab6c56c69974d137717c2540b2cb8b5ac
Zieho Choi <jeff426@naver.com>
2022-01-13 21:01:47 +0900

    worth it tbd

--- 10-grocery-bud/final/src/App.js
+++ 10-grocery-bud/final/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
+
 const getLocalStorage = () => {
   let list = localStorage.getItem('list');
   if (list) {
@@ -9,12 +10,14 @@ const getLocalStorage = () => {
     return [];
   }
 };
+
 function App() {
   const [name, setName] = useState('');
   const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
